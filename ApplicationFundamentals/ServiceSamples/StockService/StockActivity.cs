d466675d94c226e8247f2cf27b6c1557e8ce459c
Tom Opgenorth <tom@opgenorth.net>
2016-09-08 15:28:07 -0600

    Update examples, remove implicit intents, export & name services as necessary

--- ApplicationFundamentals/ServiceSamples/StockService/StockActivity.cs
+++ ApplicationFundamentals/ServiceSamples/StockService/StockActivity.cs
@@ -1,12 +1,9 @@
 using System;
-
 using Android.App;
 using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 using Android.Util;
+using Android.Widget;
 
 namespace StockService
 {
--- ApplicationFundamentals/ServiceSamples/StockService/StockService.cs
+++ ApplicationFundamentals/ServiceSamples/StockService/StockService.cs
@@ -1,17 +1,12 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net;
 using System.Json;
-
+using System.Linq;
+using System.Net;
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.Util;
 
 namespace StockService
